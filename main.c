f6dcd13cffb1032445664181e88406f2c628eb4a
RanKey1496 <jhongil96@gmail.com>
2017-08-30 23:57:27 -0500

    v1

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	double x,y,z,e;
+	printf("Enter the coefficents of the first equation of the form ax+by=c\n");
+	scanf("%lf%lf%lf",&x,&y,&z);
+	return 0;
+}
